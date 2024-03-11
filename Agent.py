6097bcb214c4a76b2bb542341aa92b48c9f9c922
G. Lunardi <g.lunardi@umail.leidenuniv.nl>
2024-03-11 22:39:59 +0100

    Agent, First environment, ReplayBuffer
    Added agent, firs try of enviornment training, example of dqn with pytorch and Replaybuffer

---
+++ Agent.py
@@ -0,0 +1,166 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Practical for master course 'Reinforcement Learning',
+Leiden University, The Netherlands
+By Thomas Moerland
+"""
+
+import numpy as np
+from Helper import softmax, argmax
+from collections import deque
+import random
+# from tensorflow.keras import models, layers, optimizers
+
+class DQNAgent:
+    """
+        DQN Agent class, with e-greedy policy and experience replay buffer
+    
+    Raises:
+        KeyError: Provide an epsilon
+        KeyError: Provide a temperature
+
+    Returns:
+        int: best action according to the policy
+    """
+    def __init__(self, n_states, n_actions, learning_rate, gamma, epsilon=0.05, epsilon_decay=0.995, epsilon_min=0.01):
+        self.n_states = n_states
+        self.n_actions = n_actions
+        self.memory = deque(maxlen=2000) 
+        self.gamma = gamma  
+        self.epsilon = epsilon
+        self.epsilon_min = epsilon_min
+        self.epsilon_decay = epsilon_decay
+        self.learning_rate = learning_rate
+        self.Q_sa = np.zeros((n_states,n_actions))
+        self.model = self.build_model()
+        
+    def select_action(self, s, policy='egreedy', epsilon=None, temp=None):
+        if policy == 'greedy':
+            '''Return greedy policy'''
+            return argmax(self.Q_sa[s, :])
+        elif policy == 'egreedy':
+            '''Return e-greedy policy'''
+            if epsilon is None:
+                raise KeyError("Provide an epsilon")
+            
+            if np.random.rand() < epsilon:
+                return np.random.randint(self.n_actions)
+            else:
+                greedy_action = np.argmax(self.Q_sa[s, :])
+                probabilities = np.ones(self.n_actions) * (epsilon / self.n_actions)
+                probabilities[greedy_action] += (1.0 - epsilon)
+                return np.random.choice(np.arange(self.n_actions), p=probabilities)
+            
+        elif policy == 'softmax':
+            '''Return Boltzmann (softmax) policy'''
+            if temp is None:
+                raise KeyError("Provide a temperature")
+            
+            q_values = self.Q_sa[s, :]
+            a = np.argmax(softmax(q_values,temp=temp))
+            return a
+
+    def build_model(self):
+        # Costruzione della rete neurale
+        model = models.Sequential()
+        model.add(layers.Dense(24, input_dim=self.state_size, activation='relu'))
+        model.add(layers.Dense(24, activation='relu'))
+        model.add(layers.Dense(self.action_size, activation='linear'))
+        model.compile(loss='mse', optimizer=optimizers.Adam(lr=self.learning_rate))
+        return model
+
+    def remember(self, state, action, reward, next_state, done):
+        self.memory.append((state, action, reward, next_state, done))
+
+    def act(self, state):
+        if np.random.rand() <= self.epsilon:
+            return random.randrange(self.action_size)
+        act_values = self.model.predict(state)
+        return np.argmax(act_values[0])
+
+    def replay(self, batch_size):
+        minibatch = random.sample(self.memory, batch_size)
+        for state, action, reward, next_state, done in minibatch:
+            target = reward
+            if not done:
+                target = (reward + self.gamma * np.amax(self.model.predict(next_state)[0]))
+            target_f = self.model.predict(state)
+            target_f[0][action] = target
+            self.model.fit(state, target_f, epochs=1, verbose=0)
+        # if self.epsilon > self.epsilon_min:
+        #     self.epsilon *= self.epsilon_decay
+    
+    def evaluate(self,eval_env,n_eval_episodes=30, max_episode_length=100):
+        returns = []  # list to store the reward per episode
+        for i in range(n_eval_episodes):
+            s = eval_env.reset()
+            R_ep = 0
+            for t in range(max_episode_length):
+                a = self.select_action(s, 'greedy')
+                s_prime, r, done = eval_env.step(a)
+                R_ep += r
+                if done:
+                    break
+                else:
+                    s = s_prime
+            returns.append(R_ep)
+        mean_return = np.mean(returns)
+        return mean_return
+
+
+# class DQNAgent:
+
+#     def __init__(self, n_states, n_actions, learning_rate, gamma):
+#         self.n_states = n_states
+#         self.n_actions = n_actions
+#         self.learning_rate = learning_rate
+#         self.gamma = gamma
+#         self.Q_sa = np.zeros((n_states,n_actions))
+        
+#     def select_action(self, s, policy='egreedy', epsilon=None, temp=None):
+#         if policy == 'greedy':
+#             '''Return greedy policy'''
+#             return argmax(self.Q_sa[s, :])
+#         elif policy == 'egreedy':
+#             '''Return e-greedy policy'''
+#             if epsilon is None:
+#                 raise KeyError("Provide an epsilon")
+            
+#             if np.random.rand() < epsilon:
+#                 return np.random.randint(self.n_actions)
+#             else:
+#                 greedy_action = np.argmax(self.Q_sa[s, :])
+#                 probabilities = np.ones(self.n_actions) * (epsilon / self.n_actions)
+#                 probabilities[greedy_action] += (1.0 - epsilon)
+#                 return np.random.choice(np.arange(self.n_actions), p=probabilities)
+            
+#         elif policy == 'softmax':
+#             '''Return Boltzmann (softmax) policy'''
+#             if temp is None:
+#                 raise KeyError("Provide a temperature")
+            
+#             q_values = self.Q_sa[s, :]
+#             a = np.argmax(softmax(q_values,temp=temp))
+#             return a
+            
+#     def update(self):
+#         raise NotImplementedError('For each agent you need to implement its specific back-up method') # Leave this and overwrite in subclasses in other files
+
+
+#     def evaluate(self,eval_env,n_eval_episodes=30, max_episode_length=100):
+#         returns = []  # list to store the reward per episode
+#         for i in range(n_eval_episodes):
+#             s = eval_env.reset()
+#             R_ep = 0
+#             for t in range(max_episode_length):
+#                 a = self.select_action(s, 'greedy')
+#                 s_prime, r, done = eval_env.step(a)
+#                 R_ep += r
+#                 if done:
+#                     break
+#                 else:
+#                     s = s_prime
+#             returns.append(R_ep)
+#         mean_return = np.mean(returns)
+#         return mean_return
---
+++ CartPole-DQN.py
@@ -0,0 +1,51 @@
+import gym
+import time
+from Agent import DQNAgent
+import numpy as np
+
+# PARAMETERS 
+num_iterations = 20000 
+
+initial_collect_steps = 100  
+collect_steps_per_iteration =   1
+replay_buffer_max_length = 100000  
+
+batch_size = 64  
+learning_rate = 1e-3  
+log_interval = 200
+
+num_eval_episodes = 10 
+eval_interval = 1000  
+
+# Entities
+env = gym.make("CartPole-v1", render_mode="human", max_episode_steps=1000)
+env_eval = gym.make("CartPole-v1")
+dqn_agent = DQNAgent(n_states=env.observation_space.shape[0], 
+                     n_actions=env.action_space, 
+                     learning_rate=learning_rate, 
+                     gamma=0.99)
+replay_buffer = [] 
+observation, info = env.reset(seed=42)
+# first_eval = 
+
+for iteration in range(num_iterations):
+    state = env.reset()
+    state = np.reshape(state, [1, dqn_agent.n_states])
+    done = False
+    
+    while not done:
+        # Seleziona l'azione
+        action = dqn_agent.select_action(state)
+        observation, reward, terminated, truncated, info = env.step(action)
+        next_state = np.reshape(next_state, [1, dqn_agent.n_states])
+
+        # Memorizza l'esperienza
+        dqn_agent.remember(state, action, reward, next_state, done)
+
+        state = next_state
+
+        if len(dqn_agent.memory) > batch_size:
+            # Allenamento con esperienze praecedenti
+            dqn_agent.replay(batch_size)
+
+env.close()
