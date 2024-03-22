ba921a7b9b6d0caed9cc1a5e42f6351a2e7dc144
Gio200023 <lunardigiovanni@icloud.com>
2024-03-22 14:52:58 +0100

    Removed comments and add smooth

--- CartPoleDQN.py
+++ CartPoleDQN.py
@@ -23,6 +23,7 @@ temp = 0.05
 
 def dqn(n_timesteps=num_iterations, use_replay_buffer=True, learning_rate=learning_rate, gamma=gamma, 
         policy="egreedy", epsilon=epsilon, temp=temp, eval_interval=eval_interval, batch_size=batch_size):
+    
     # Entities
     env = gym.make("CartPole-v1", max_episode_steps=1000)
     env_eval = gym.make("CartPole-v1")
@@ -44,13 +45,12 @@ def dqn(n_timesteps=num_iterations, use_replay_buffer=True, learning_rate=learni
     iteration = 0
     while iteration <= n_timesteps:
         state, info = env.reset()
-        # state = np.reshape(state, [1, dqn_agent_and_model.n_states])
+
         terminated = False
         while not terminated:
             action = dqn_agent_and_model.select_action(state,policy=policy)
             observation, reward, terminated, truncated, info = env.step(action)
-            # observation = np.reshape(observation, [1, dqn_agent_and_model.n_states])
-            # reward = reward / 100
+
             if use_replay_buffer:
                 dqn_agent_and_model.remember(state, action, reward, observation, terminated)
                 if len(dqn_agent_and_model.replay_buffer) > batch_size:
@@ -63,7 +63,6 @@ def dqn(n_timesteps=num_iterations, use_replay_buffer=True, learning_rate=learni
             if iteration % eval_interval == 0:
                 eval_timesteps.append(iteration)
                 eval_returns.append(dqn_agent_and_model.evaluate(env_eval, n_eval_episodes=num_eval_episodes, epsilon = epsilon, temp = temp))
-                # print(f"Average return at iteration {iteration}: {eval_returns}")
                 print("step: ",iteration)
             iteration+=1
             dqn_agent_and_model._current_iteration=iteration
