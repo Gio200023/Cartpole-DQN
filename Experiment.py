39b8095e112d1784d24ba16623bce967eed0bbfb
Yağmur DOĞAN <85808317+yagmurdogan8@users.noreply.github.com>
2024-03-28 17:57:54 +0300

    false label it should be dqn-tn-er

--- Experiment.py
+++ Experiment.py
@@ -79,7 +79,7 @@ def experiment(use_replay_buffer, use_target_network):
     # Back-up & update
     learning_rates = [0.01,0.001,0.1]
     
-    Plot = LearningCurvePlot(title = "DQN-ER")
+    Plot = LearningCurvePlot(title = "DQN-TR-ER")
     Plot.set_ylim(0, 500) 
     for learning_rate in learning_rates:
         for policy in policies:
@@ -90,7 +90,7 @@ def experiment(use_replay_buffer, use_target_network):
             
             Plot.add_curve(timesteps,learning_curve,label=(str(learning_rate)+","+str(policy)))
             
-    Plot.save('dqn.png')
+    Plot.save('dqn_no_tr_no_er.png')
 
 if __name__ == '__main__':
     args = get_args()
